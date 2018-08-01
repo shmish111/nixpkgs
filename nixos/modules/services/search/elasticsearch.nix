7fc1a28c98896314d12ec9bcb028692a1552b444
David Smith <shmish111@gmail.com>
2018-08-01 09:35:16 +0100

    nixos/elasticsearch: add curator option
    https://www.elastic.co/guide/en/elasticsearch/client/curator/current/index.html
    
    A very useful utility that does some very common tasks that you
    probably should have running on your ES cluster.

--- nixos/modules/services/search/elasticsearch.nix
+++ nixos/modules/services/search/elasticsearch.nix
@@ -154,6 +154,40 @@ in {
       type = types.listOf types.package;
     };
 
+    curatorCli = mkOption {
+      description = "a curator-cli command, alternatively use curator which takes a full action file";
+      example = ''
+        delete_indices --ignore_empty_list --filter_list '[{"filtertype":"age","source":"creation_date","direction":"older","unit":"days","unit_count":45}]'
+      '';
+    };
+
+    curator = mkOption {
+      description = "curator action.yaml file contents, alternatively use curator-cli which takes a simple action command";
+      example = ''
+        ---
+        actions:
+          1:
+            action: delete_indices
+            description: >-
+              Delete indices older than 45 days (based on index name), for logstash-
+              prefixed indices. Ignore the error if the filter does not result in an
+              actionable list of indices (ignore_empty_list) and exit cleanly.
+            options:
+              ignore_empty_list: True
+              disable_action: True
+            filters:
+            - filtertype: pattern
+              kind: prefix
+              value: logstash-
+            - filtertype: age
+              source: name
+              direction: older
+              timestring: '%Y.%m.%d'
+              unit: days
+              unit_count: 45
+      '';
+    };
+
   };
 
   ###### implementation
@@ -224,5 +258,47 @@ in {
         group = "elasticsearch";
       };
     };
-  };
+  }
+  // (optionalAttrs (cfg.curatorCli != null) {
+          systemd.services.curator-cli = {
+            enable = true;
+            startAt = "hourly";
+            serviceConfig = {
+            ExecStart = ''${pkgs.python36Packages.elasticsearch-curator}/bin/curator_cli ${cfg.curatorCli}'';
+            };
+          };
+      })
+  // (optionalAttrs (cfg.curator != null) {
+          environment.etc."curator/config.yaml" = { text = ''
+            ---
+            # Remember, leave a key empty if there is no value.  None will be a string,
+            # not a Python "NoneType"
+            client:
+              hosts:
+                - 127.0.0.1
+              port: 9200
+              url_prefix:
+              use_ssl: False
+              certificate:
+              client_cert:
+              client_key:
+              ssl_no_validate: False
+              http_auth:
+              timeout: 30
+              master_only: False
+            logging:
+              loglevel: INFO
+              logfile:
+              logformat: default
+              blacklist: ['elasticsearch', 'urllib3']
+            ''; };
+          environment.etc."curator/action.yaml" = { text = cfg.curator; };
+          systemd.services.curator = {
+            enable = true;
+            startAt = "hourly";
+            serviceConfig = {
+            ExecStart = ''${pkgs.python36Packages.elasticsearch-curator}/bin/curator ${cfg.curator}'';
+            };
+          };
+      });
 }
