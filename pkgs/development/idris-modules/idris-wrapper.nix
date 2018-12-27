37573895c9afcb400aca74c57278093b6fd22864
David Smith <shmish111@gmail.com>
2018-12-27 00:35:21 -0200

    try again

--- pkgs/development/idris-modules/idris-wrapper.nix
+++ pkgs/development/idris-modules/idris-wrapper.nix
@@ -8,5 +8,6 @@ symlinkJoin {
     wrapProgram $out/bin/idris \
       --run 'export IDRIS_CC=''${IDRIS_CC:-${stdenv.cc}/bin/cc}'
       --suffix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ gmp ]}
+      --suffix NIX_CPATH_LINK : ${lib.makeLibraryPath [ gmp ]}
   '';
 }
