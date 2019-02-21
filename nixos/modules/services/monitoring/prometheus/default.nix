c73222f0ef9ba859f72e5ea2fb16e3f0e0242492
Alberto Berti <alberto@metapensiero.it>
2019-02-21 15:29:54 +0100

    Add prometheus2 configuration to the prometheus modules
    As the configuration for the exporters and alertmanager is unchanged
    between the two major versions this patch tries to minimize
    duplication while at the same time as there's no upgrade path from 1.x
    to 2.x, it allows running the two services in parallel. See also #56037
    
    Make it pass a minimal test
    
    Fix alertmanager service definition. Thanks to @eonpatapon
    
    Rollback versionning of services.prometheus.{exporters, alertmanager}.
    
    Prometheus2: --web.external-url need two dash.
    
    Use same user for both prometheus 1 and 2. Use StateDirectory.
    
    nixos/prometheus: use ExecStart instead of a shell script
    
    This uses fewer lines of code and one less process.
    
    nixos/prometheus: get rid of empty arguments
    
    Previously the prometheus.service file looked like:
    
      ExecStart=/nix/store/wjkhfw3xgkmavz1akkqir99w4lbqhak7-prometheus-1.8.2-bin/bin/prometheus -storage.local.path=/var/lib/prometheus/metrics \
        -config.file=/nix/store/zsnvzw51mk3n1cxjd0351bj39k1j6j27-prometheus.yml-check-config-checked \
        -web.listen-address=0.0.0.0:9090 \
        -alertmanager.notification-queue-capacity=10000 \
        -alertmanager.timeout=10s \
         \
    
      Restart=always
    
    Now it's:
    
      ExecStart=/nix/store/wjkhfw3xgkmavz1akkqir99w4lbqhak7-prometheus-1.8.2-bin/bin/prometheus \
        -storage.local.path=/var/lib/prometheus/metrics \
        -config.file=/nix/store/zsnvzw51mk3n1cxjd0351bj39k1j6j27-prometheus.yml-check-config-checked \
        -web.listen-address=0.0.0.0:9090 \
        -alertmanager.notification-queue-capacity=10000 \
        -alertmanager.timeout=10s
      Restart=always
    
    nixos/tests: register the prometheus2 test

--- nixos/modules/services/monitoring/prometheus/default.nix
+++ nixos/modules/services/monitoring/prometheus/default.nix
@@ -4,6 +4,7 @@ with lib;
 
 let
   cfg = config.services.prometheus;
+  cfg2 = config.services.prometheus2;
   promUser = "prometheus";
   promGroup = "prometheus";
 
@@ -25,13 +26,23 @@ let
     promtool ${promtoolCommand what} $out
   '';
 
+  # a wrapper that verifies that the configuration is valid for
+  # prometheus 2
+  prom2toolCheck = what: name: file:
+    pkgs.runCommand
+      "${name}-${replaceStrings [" "] [""] what}-checked"
+      { buildInputs = [ cfg2.package ]; } ''
+    ln -s ${file} $out
+    promtool ${what} $out
+  '';
+
   # Pretty-print JSON to a file
   writePrettyJSON = name: x:
     pkgs.runCommand name { preferLocalBuild = true; } ''
       echo '${builtins.toJSON x}' | ${pkgs.jq}/bin/jq . > $out
     '';
 
-  # This becomes the main config file
+  # This becomes the main config file for Prometheus 1
   promConfig = {
     global = cfg.globalConfig;
     rule_files = map (promtoolCheck "check-rules" "rules") (cfg.ruleFiles ++ [
@@ -43,30 +54,53 @@ let
   generatedPrometheusYml = writePrettyJSON "prometheus.yml" promConfig;
 
   prometheusYml = let
-    yml =  if cfg.configText != null then
+    yml = if cfg.configText != null then
       pkgs.writeText "prometheus.yml" cfg.configText
       else generatedPrometheusYml;
     in promtoolCheck "check-config" "prometheus.yml" yml;
 
   cmdlineArgs = cfg.extraFlags ++ [
-    (if isVersionTwo then "--storage.tsdb.path=${cfg.dataDir}/metrics" else "-storage.local.path=${cfg.dataDir}/metrics")
-    (if isVersionTwo then "--config.file=${prometheusYml}" else "-config.file=${prometheusYml}")
-    (if isVersionTwo then "--web.listen-address=${cfg.listenAddress}" else "-web.listen-address=${cfg.listenAddress}")
-    (if isVersionTwo
-      then "--alertmanager.notification-queue-capacity=${toString cfg.alertmanagerNotificationQueueCapacity}"
-      else "-alertmanager.notification-queue-capacity=${toString cfg.alertmanagerNotificationQueueCapacity}")
-    (if isVersionTwo
-      then "--alertmanager.timeout=${toString cfg.alertmanagerTimeout}s"
-      else "-alertmanager.timeout=${toString cfg.alertmanagerTimeout}s")
-    (optionalString (cfg.alertmanagerURL != []) 
-      (if isVersionTwo 
-        then "--alertmanager.url=${concatStringsSep "," cfg.alertmanagerURL}"
-        else "-alertmanager.url=${concatStringsSep "," cfg.alertmanagerURL}"))
-    (optionalString (cfg.webExternalUrl != null) 
-      (if isVersionTwo
-        then "--web.external-url=${cfg.webExternalUrl}"
-        else "-web.external-url=${cfg.webExternalUrl}"))
-  ];
+    "-storage.local.path=/var/lib/prometheus/metrics"
+    "-config.file=${prometheusYml}"
+    "-web.listen-address=${cfg.listenAddress}"
+    "-alertmanager.notification-queue-capacity=${toString cfg.alertmanagerNotificationQueueCapacity}"
+    "-alertmanager.timeout=${toString cfg.alertmanagerTimeout}s"
+  ] ++
+    (optional (cfg.alertmanagerURL != []) "-alertmanager.url=${concatStringsSep "," cfg.alertmanagerURL}") ++
+    (optional (cfg.webExternalUrl != null) "-web.external-url=${cfg.webExternalUrl}");
+
+  # This becomes the main config file for Prometheus 2
+  promConfig2 = {
+    global = cfg2.globalConfig;
+    rule_files = map (prom2toolCheck "check rules" "rules") (cfg2.ruleFiles ++ [
+      (pkgs.writeText "prometheus.rules" (concatStringsSep "\n" cfg2.rules))
+    ]);
+    scrape_configs = cfg2.scrapeConfigs;
+    alerting = optionalAttrs (cfg2.alertmanagerURL != []) {
+      alertmanagers = [{
+        static_configs = [{
+          targets = cfg2.alertmanagerURL;
+        }];
+      }];
+    };
+  };
+
+  generatedPrometheus2Yml = writePrettyJSON "prometheus.yml" promConfig2;
+
+  prometheus2Yml = let
+    yml = if cfg2.configText != null then
+      pkgs.writeText "prometheus.yml" cfg2.configText
+      else generatedPrometheus2Yml;
+    in prom2toolCheck "check config" "prometheus.yml" yml;
+
+  cmdlineArgs2 = cfg2.extraFlags ++ [
+    "--storage.tsdb.path=/var/lib/prometheus2/data/"
+    "--config.file=${prometheus2Yml}"
+    "--web.listen-address=${cfg2.listenAddress}"
+    "--alertmanager.notification-queue-capacity=${toString cfg2.alertmanagerNotificationQueueCapacity}"
+    "--alertmanager.timeout=${toString cfg2.alertmanagerTimeout}s"
+  ] ++
+    (optional (cfg2.webExternalUrl != null) "--web.external-url=${cfg2.webExternalUrl}");
 
   promTypes.globalConfig = types.submodule {
     options = {
@@ -420,14 +454,6 @@ in {
         '';
       };
 
-      dataDir = mkOption {
-        type = types.path;
-        default = "/var/lib/prometheus";
-        description = ''
-          Directory to store Prometheus metrics data.
-        '';
-      };
-
       extraFlags = mkOption {
         type = types.listOf types.str;
         default = [];
@@ -515,30 +541,160 @@ in {
         '';
       };
     };
-  };
+    services.prometheus2 = {
 
-  config = mkIf cfg.enable {
-    users.groups.${promGroup}.gid = config.ids.gids.prometheus;
-    users.users.${promUser} = {
-      description = "Prometheus daemon user";
-      uid = config.ids.uids.prometheus;
-      group = promGroup;
-      home = cfg.dataDir;
-      createHome = true;
-    };
-    systemd.services.prometheus = {
-      wantedBy = [ "multi-user.target" ];
-      after    = [ "network.target" ];
-      script = ''
-        #!/bin/sh
-        exec ${cfg.package}/bin/prometheus \
-          ${concatStringsSep " \\\n  " cmdlineArgs}
-      '';
-      serviceConfig = {
-        User = promUser;
-        Restart  = "always";
-        WorkingDirectory = cfg.dataDir;
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Enable the Prometheus 2 monitoring daemon.
+        '';
+      };
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.prometheus_2;
+        defaultText = "pkgs.prometheus_2";
+        description = ''
+          The prometheus2 package that should be used.
+        '';
+      };
+
+      listenAddress = mkOption {
+        type = types.str;
+        default = "0.0.0.0:9090";
+        description = ''
+          Address to listen on for the web interface, API, and telemetry.
+        '';
+      };
+
+      extraFlags = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        description = ''
+          Extra commandline options when launching Prometheus 2.
+        '';
+      };
+
+      configText = mkOption {
+        type = types.nullOr types.lines;
+        default = null;
+        description = ''
+          If non-null, this option defines the text that is written to
+          prometheus.yml. If null, the contents of prometheus.yml is generated
+          from the structured config options.
+        '';
+      };
+
+      globalConfig = mkOption {
+        type = promTypes.globalConfig;
+        default = {};
+        apply = _filter;
+        description = ''
+          Parameters that are valid in all  configuration contexts. They
+          also serve as defaults for other configuration sections
+        '';
+      };
+
+      rules = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        description = ''
+          Alerting and/or Recording rules to evaluate at runtime.
+        '';
+      };
+
+      ruleFiles = mkOption {
+        type = types.listOf types.path;
+        default = [];
+        description = ''
+          Any additional rules files to include in this configuration.
+        '';
+      };
+
+      scrapeConfigs = mkOption {
+        type = types.listOf promTypes.scrape_config;
+        default = [];
+        apply = x: map _filter x;
+        description = ''
+          A list of scrape configurations.
+        '';
+      };
+
+      alertmanagerURL = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        description = ''
+          List of Alertmanager URLs to send notifications to.
+        '';
+      };
+
+      alertmanagerNotificationQueueCapacity = mkOption {
+        type = types.int;
+        default = 10000;
+        description = ''
+          The capacity of the queue for pending alert manager notifications.
+        '';
+      };
+
+      alertmanagerTimeout = mkOption {
+        type = types.int;
+        default = 10;
+        description = ''
+          Alert manager HTTP API timeout (in seconds).
+        '';
+      };
+
+      webExternalUrl = mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        example = "https://example.com/";
+        description = ''
+          The URL under which Prometheus is externally reachable (for example,
+          if Prometheus is served via a reverse proxy).
+        '';
       };
     };
-  };
+   };
+
+  config = mkMerge [
+    (mkIf (cfg.enable || cfg2.enable) {
+      users.groups.${promGroup}.gid = config.ids.gids.prometheus;
+      users.users.${promUser} = {
+        description = "Prometheus daemon user";
+        uid = config.ids.uids.prometheus;
+        group = promGroup;
+      };
+    })
+    (mkIf cfg.enable {
+      systemd.services.prometheus = {
+        wantedBy = [ "multi-user.target" ];
+        after    = [ "network.target" ];
+        serviceConfig = {
+        ExecStart = "${cfg.package}/bin/prometheus" +
+          optionalString (length cmdlineArgs != 0) (" \\\n  " +
+            concatStringsSep " \\\n  " cmdlineArgs);
+          User = promUser;
+          Restart  = "always";
+          WorkingDirectory = /var/lib/prometheus;
+          StateDirectory = "prometheus";
+        };
+      };
+    })
+    (mkIf cfg2.enable {
+      systemd.services.prometheus2 = {
+        wantedBy = [ "multi-user.target" ];
+        after    = [ "network.target" ];
+        serviceConfig = {
+          ExecStart = "${cfg2.package}/bin/prometheus" +
+            optionalString (length cmdlineArgs2 != 0) (" \\\n  " +
+              concatStringsSep " \\\n  " cmdlineArgs2);
+          User = promUser;
+          Restart  = "always";
+          WorkingDirectory = /var/lib/prometheus2;
+          StateDirectory = "prometheus2";
+        };
+      };
+    })
+  ];
 }
